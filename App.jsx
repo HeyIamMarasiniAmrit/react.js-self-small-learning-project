4bd434c70ecb593b57b32e6b771b83e3122fdee5
Amrit Marasini <101445427+HeyIamMarasiniAmrit@users.noreply.github.com>
2025-11-27 19:42:19 +0545

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,28 +1,44 @@
 import React, { useState } from 'react';
-"Eat", "code", "sleep","Repeat"
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import './index.css';
+const App = () => {
+  const [p, setP] =  useState(0)
+   const [t, setT] =  useState(0)
+    const [r, setR] =  useState(0)
+     const [si, setSI] =  useState(0)
 
-
-const App = () =>{
-  const [userInput, setUserInput] = useState([])
-  const [todos, setTodos] = useState([])
-
-  const addData = () =>{
-  setTodos([...todos, userInput])
-  }
-
-  return <div>
-
-    <h1>Todo List</h1>
-    <input onChange={(event)=> setUserInput(event.target.value)} type="text"></input><br/><br/>
-    <button onClick={() => addData()}>Add</button>
-    <ul>
-     {
-      todos.map((todo, index)=> <li key= {index}>{todo}</li>)
+     const CalculateInterest = () =>{
+      setSI(p*t*r/100)
      }
-
-    </ul>
+ return (
+  <div className='div'>
+<Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Simple Interest Calculator 
+          </Typography>
+          
+        </Toolbar>
+      </AppBar>
+      <br/>
+      <TextField onChange={(event) => setP(event.target.value)} id="outlined-basic" label="Enter P" variant="outlined" /><br/><br/>
+      <TextField onChange={(event) => setT(event.target.value)} id="outlined-basic" label="Enter T" variant="outlined" /><br/><br/>
+      <TextField onChange={(event) => setR(event.target.value)} id="outlined-basic" label="Enter R" variant="outlined" /><br/><br/>
+      <br/><br/>
+      <Button onClick={()=> CalculateInterest()} variant="contained">Calculate</Button>
+      <Typography variant="h6" gutterBottom>
+        Simple Interest Is: {si}
+      </Typography>
+    </Box>
   </div>
+ );
 
 }
-
 export default App;
