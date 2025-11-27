72026d1560a072b906025c775a526b168342a000
Amrit Marasini <101445427+HeyIamMarasiniAmrit@users.noreply.github.com>
2025-11-27 19:47:28 +0545

    Add files via upload

---
+++ ItemQuotes.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: (theme.vars ?? theme).palette.text.secondary,
+  ...theme.applyStyles('dark', {
+    backgroundColor: '#1A2027',
+  }),
+}));
+
+const ItemQuotes = ({text, author}) => {
+  return (
+    <div>
+       <Item>{text}</Item><pre>Author: {author}</pre>
+    </div>
+  )
+}
+
+export default ItemQuotes
